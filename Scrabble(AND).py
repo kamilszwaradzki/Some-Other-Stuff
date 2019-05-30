ba6695aec18382fe8ef8732d3d4d5570fef985f1
Kamil Szwaradzki <kamil.szwaradzki@gmail.com>
2019-05-30 22:01:31 +0200

    Update Scrabble(AND).py

--- Scrabble(AND).py
+++ Scrabble(AND).py
@@ -3,7 +3,7 @@ letters = input("Jakie literki tam sie zawieraja? ")
 count_word = int(input("Iloliterowe jest to slowo? "))
 words = {''.join(x) for x in permutations(letters,count_word)}
 
-with open('/storage/emulated/0/Download/slowa.txt') as f:
+with open('/storage/emulated/0/Download/slowa.txt') as f: # https://sjp.pl/slownik/growy/sjp-20190528.zip
 	for linia in f:
 		if linia.strip() in words:
 			if len(linia.strip()) == count_word:
