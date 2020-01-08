ff12b8d36eb8a3afaa9e2a5ee90bab49c1afd501
Kamil Szwaradzki <kamil.szwaradzki@gmail.com>
2020-01-08 12:32:13 +0000

    For Android's Python interpreter.

--- Scrabble(AND).py
+++ Scrabble(AND).py
@@ -4,7 +4,7 @@ count_word = int(input("Iloliterowe jest to slowo? "))
 words = {''.join(x) for x in permutations(letters,count_word)}
 
 with open('/storage/emulated/0/Download/slowa.txt') as f: # https://sjp.pl/slownik/growy/sjp-20190528.zip
-	for linia in f:
-		if linia.strip() in words:
-			if len(linia.strip()) == count_word:
-				print (linia.strip())
+	for line in f:
+		if line.strip() in words:
+			if len(line.strip()) == count_word:
+				print (line.strip())
