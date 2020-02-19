c6acbc3c75f0454a0ea2181e9e7fff275b522a31
Kamil Szwaradzki <kamil.szwaradzki@gmail.com>
2020-02-19 22:52:58 +0000

    Adding GUI with clickable links.

--- ScrapLowcygierDotpl.py
+++ ScrapLowcygierDotpl.py
@@ -12,24 +12,37 @@ how to run the script with a doubleclick? (work with Gnome)
 
 from bs4 import BeautifulSoup
 import requests
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
 
 soup = BeautifulSoup(requests.get(r"https://lowcygier.pl/darmowe/").text,"html.parser")
-y = {x.text:x.get('href') for x in soup.find_all("a") if  "darmo" in str(x)}
-y = { x:y[x] for x in y.keys()  if ('Store' in x or 'GOG' in x or 'Steam' in x) and 'darmowy' not in x and 'Darmowy' not in x}
-print(y.keys())
 
+class LinkButtonWindow(Gtk.Window):
 
-"""
-screen = Tk()
-text = Text(screen)
-text.pack()
-text.insert(END,r.json()['data']['file']['url']['short'] if r.json()["status"] == True else r.text)
+    def __init__(self):
+        Gtk.Window.__init__(self, title="List of free games")
+        self.set_border_width(10)
+        box_outer = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.add(box_outer)
 
-def apress(): #copy
-    pyperclip.copy(r.json()['data']['file']['url']['short'])
+        listbox = Gtk.ListBox()
+        listbox.set_selection_mode(Gtk.SelectionMode.NONE)
+        box_outer.pack_start(listbox, True, True, 0)
 
-btn = Button(screen, text = 'copy', width = 5, command = apress) 
-btn.pack()
+        row = Gtk.ListBoxRow()
+        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
+        row.add(hbox)
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        hbox.pack_start(vbox, True, True, 0)
+        
+        y = {x.text:x.get('href') for x in soup.find_all("a") if  "darmo" in str(x)}
+        y = { x:y[x] for x in y.keys()  if ('Store' in x or 'GOG' in x or 'Steam' in x) and 'darmowy' not in x and 'Darmowy' not in x}
+        for x in y.keys():
+            vbox.pack_start(Gtk.LinkButton(y[x], x,  xalign=0), True, True, 0)
+        listbox.add(row)
 
-
-mainloop() """
+win = LinkButtonWindow()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
